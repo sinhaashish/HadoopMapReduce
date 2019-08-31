bb9cd0c6703aeed70a3f3e0f041a81ffc60f04ca
Ashish Kumar Sinha <ashi.sinha.87@gmail.com>
2019-08-31 23:45:33 +0530

    Update AppData.java

--- AppData.java
+++ AppData.java
@@ -2,7 +2,7 @@
 // WARNING: This class is AUTO-GENERATED. Modify at your own risk.
 // adding extra line for git changes
 import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.lib.db.DBWritable;
 import com.cloudera.sqoop.lib.JdbcWritableBridge;
